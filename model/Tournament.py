fff89e0ff6241ec6b3ede77dd44e6387e0b6453c
babut1 <93227742+babut1@users.noreply.github.com>
2022-03-31 08:32:08 +0200

    Update Tournament.py

--- model/Tournament.py
+++ model/Tournament.py
@@ -7,6 +7,19 @@ class Tournament:
         self.__params_list = []
         self.__rounds_list = []
 
+    #Consstructor assumes number of runds as parameter
+
+    def __init__(self, maxRounds):
+        self.__params_list = []
+        self.__rounds_list = []
+        self.maxRounds = maxRounds
+
+    def __init__(self, maxRounds):
+        self.__params_list = []
+        self.__rounds_list = []
+        self.maxRounds = maxRounds
+        self.scores = [ [-1] * maxRounds for _ in range(maxRounds)]
+
     def add_player(self, player):
         if not isinstance(player, Player):
             raise TypeError()
@@ -16,3 +29,65 @@ class Tournament:
     def next_round(self):
         next_round = Round(self, len(self.__rounds_list) + 1)
         self.__rounds_list += next_round
+
+
+    #Function count_scores counts number of points of a player(playerID)
+    def count_scores(self, playerID):
+        sum = 0
+        for i in range(len(self.__params_list)):
+            if self.scores[i][playerID] != -1:
+                sum += self.scores[i][playerID]
+        return sum
+
+
+    #Function count_Buholtz counts Buholtz of a player(playerID)
+
+    def count_Buholtz(self, playerID):
+        sum = 0
+        for i in range(len(self.__params_list)):
+            if self.scores[i][playerID] != -1:
+                sum += self.count_scores(self, playerID)
+        return sum
+
+
+    #Sorts tuple by given element
+
+    def Sort_Tuple(tup, elem): 
+    
+        tup.sort(key = lambda x: x[elem], reverse = True) 
+        return tup 
+
+
+    #Function get_scores counts points and Buholtz of all the players than sorts the scores, returns current scores
+
+    def get_scores(self):
+        points = []
+        for i in range(len(self.__params_list)):
+            points.append((i, self.count_scores(self, i), self.count_Buholtz(self,i)))
+        self.Sort_Tuple(points, 1)
+        for i in range(len(self.__params_list)-1):
+            if points[i][1] == points[i+1][1]:
+                if self.count_Buholtz(self, i+1) > self.count_Buholtz(self, i):
+                    points[i], points[i+1] = points[i+1], points[i]
+        for i in range(len(self.__params_list)-1):
+            if points[i][1] == points[i+1]:
+                if points[i][2] == points[i+1][2]:
+                    if self.scores[points[i][0]][points[i+1][0]] == 0:
+                        points[i], points[i+1] = points[i+1], points[i]
+        return points
+
+    #Function prints scores
+
+    def print_scores(self):
+        print(self.get_scores())
+
+    #Function updates the scores 
+
+    def set_match_score(self, match):
+        self.scores[match.player_white.playerID][match.player_black.playerID] = match.result
+        if match.result == 0:
+            self.scores[match.player_black.playerID][match.player_white.playerID] == 1
+        if match.result == 1:
+            self.scores[match.player_black.playerID][match.player_white.playerID] == 0
+        if match.result == 0.5:
+            self.scores[match.player_black.playerID][match.player_white.playerID] == 0.5 
