5e228f02714e8312ed556da2b5b92208dbc3e38a
babut1 <93227742+babut1@users.noreply.github.com>
2022-03-31 08:31:39 +0200

    Update PlayerParams.py

--- model/PlayerParams.py
+++ model/PlayerParams.py
@@ -1,9 +1,19 @@
 class PlayerParams:
 
-    def __init__(self, player):
+    def __init__(self, player, playerID):
         self.__player = player
         self.__points = 0
         self.__buchholz = 0
+        self.__playerID = playerID
+
+    #In tournaments with odd player number
+
+    def __init__(self, player, playerID):
+        self.__player = player
+        self.__points = 0
+        self.__buchholz = 0
+        self.__playerID = playerID
+        self.__didPause = False
 
     def set_points(self, points):
         if points < 0:
@@ -23,3 +33,12 @@ class PlayerParams:
 
     def get_player(self):
         return self.__player
+
+    def get_playerID(self):
+        return self.__playerID
+
+    def set_didPause(self):
+        self.__didPause = True
+
+    def get_didPause(self):
+        return self.__didPause
