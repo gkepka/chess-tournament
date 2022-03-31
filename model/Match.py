9c195f385020383d81d878a3d647b5b6f61ddc47
babut1 <93227742+babut1@users.noreply.github.com>
2022-03-31 08:30:49 +0200

    Update Match.py

--- model/Match.py
+++ model/Match.py
@@ -4,3 +4,18 @@ class Match:
         self.player_white = player_white
         self.player_black = player_black
         self.result = None
+
+    #Result from white player perspective
+
+    def __init__(self, player_white, player_black, result):
+        self.player_white = player_white
+        self.player_black = player_black
+        self.result = result
+
+    def get_result(self):
+        return self.result
+
+
+    def set_result(self, result):
+        self.result = result
+
