f7e201fec2626ae3c7f071b6d40a8c9e64b51241
Gabriel Kępka <gkepka@student.agh.edu.pl>
2022-04-28 22:37:03 +0200

    Added PlayerDAO

--- model/Player.py
+++ model/Player.py
@@ -1,8 +1,9 @@
 class Player:
 
-    def __init__(self, name, surname, rank, nationality, title):
+    def __init__(self, name, surname, rank, nationality, title, player_id = None):
         self.name = name
         self.surname = surname
         self.rank = rank
         self.nationality = nationality
         self.title = title
+        self.player_id = player_id
