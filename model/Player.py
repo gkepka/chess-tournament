bfaac8bc448a82b5dea7028561db9d1e20fda1a0
babut1 <93227742+babut1@users.noreply.github.com>
2022-05-13 02:12:58 +0200

    Update Player.py

--- model/Player.py
+++ model/Player.py
@@ -1,9 +1,10 @@
 class Player:
 
-    def __init__(self, name, surname, rank, nationality, title, player_id=None):
+    def __init__(self, name, surname, rank, nationality, title, chess_club, player_id=None):
         self.name = name
         self.surname = surname
         self.rank = rank
         self.nationality = nationality
         self.title = title
         self.player_id = player_id
+        self.chess_club = chess_club
